2f9c2d536737fac910c5440173732171407b8b74
kendy <kendypard1999@hotmail.com>
2024-01-27 15:25:03 -0400

    we

---
+++ src/main/java/net/kendy/modtestarmory/Modtestarmory.java
@@ -0,0 +1,17 @@
+package net.kendy.modtestarmory;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Modtestarmory implements ModInitializer {
+	public static final String MOD_ID = "modtestarmory";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/kendy/modtestarmory/ModtestarmoryClient.java
@@ -0,0 +1,11 @@
+package net.kendy.modtestarmory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModtestarmoryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
